df4e1a870daa3b415e94b3a6825370f0bdb7a776
Steven G <estengrove99@gmail.com>
2020-01-29 21:52:56 -0700

    SUBJECT: Added "outside click" to close to modals.
    	Description:
    		- "Outside click to close" is now embedded in each modal component.
    
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/components/shared/Modal.js
    	modified:   src/components/shared/ModalSM.js

--- src/components/shared/Modal.js
+++ src/components/shared/Modal.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { useOutsideClick } from "../../utils/useOutsideClick";
 import { PropTypes } from "prop-types";
 import styles from "../../css/shared/Modal.module.scss";
 import sprite from "../../assets/buttons.svg";
 
 const Modal = ({ title, closeModal, children }) => {
-  return (
-    <aside className={styles.Modal}>
-      <section className={styles.Modal_top}>
-        <h4 className={styles.Modal_top_title}>{title}</h4>
-        <svg className={styles.Modal_top_icon} onClick={closeModal}>
-          <use xlinkHref={`${sprite}#icon-clearclose`}></use>
-        </svg>
-      </section>
-      <section className={styles.Modal_main}>{children}</section>
-    </aside>
-  );
+	const modalRef = useRef();
+	const { isOutside } = useOutsideClick(modalRef);
+
+	useEffect(() => {
+		let isMounted = true;
+		if (!isMounted) {
+			return;
+		}
+		if (isOutside) {
+			return closeModal();
+		}
+		return () => {
+			isMounted = false;
+		};
+	}, [isOutside, closeModal]);
+	return (
+		<aside className={styles.Modal} ref={modalRef}>
+			<section className={styles.Modal_top}>
+				<h4 className={styles.Modal_top_title}>{title}</h4>
+				<svg className={styles.Modal_top_icon} onClick={closeModal}>
+					<use xlinkHref={`${sprite}#icon-clearclose`}></use>
+				</svg>
+			</section>
+			<section className={styles.Modal_main}>{children}</section>
+		</aside>
+	);
 };
 
 export default Modal;
 
 Modal.propTypes = {
-  title: PropTypes.string,
-  closeModal: PropTypes.func,
-  children: PropTypes.element
+	title: PropTypes.string,
+	closeModal: PropTypes.func,
+	children: PropTypes.element
 };
--- src/components/shared/ModalSM.js
+++ src/components/shared/ModalSM.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { PropTypes } from "prop-types";
+import { useOutsideClick } from "../../utils/useOutsideClick";
 import styles from "../../css/shared/ModalSM.module.scss";
 import sprite from "../../assets/buttons.svg";
 
 const ModalSM = ({ title, closeModal, children }) => {
-  return (
-    <aside className={styles.ModalSM}>
-      <header className={styles.ModalSM_header}>
-        <h2 className={styles.ModalSM_header_title}>{title}</h2>
-        <svg className={styles.ModalSM_header_closeIcon} onClick={closeModal}>
-          <use xlinkHref={`${sprite}#icon-clearclose`}></use>
-        </svg>
-      </header>
-      <section className={styles.ModalSM_main}>{children}</section>
-    </aside>
-  );
+	const modalSmRef = useRef();
+	const { isOutside } = useOutsideClick(modalSmRef);
+
+	useEffect(() => {
+		let isMounted = true;
+		if (!isMounted) {
+			return;
+		}
+		if (isOutside) {
+			return closeModal();
+		}
+		return () => {
+			isMounted = false;
+		};
+	}, [isOutside, closeModal]);
+
+	return (
+		<aside className={styles.ModalSM} ref={modalSmRef}>
+			<header className={styles.ModalSM_header}>
+				<h2 className={styles.ModalSM_header_title}>{title}</h2>
+				<svg className={styles.ModalSM_header_closeIcon} onClick={closeModal}>
+					<use xlinkHref={`${sprite}#icon-clearclose`}></use>
+				</svg>
+			</header>
+			<section className={styles.ModalSM_main}>{children}</section>
+		</aside>
+	);
 };
 
 export default ModalSM;
@@ -22,7 +39,7 @@ export default ModalSM;
 ModalSM.defaultProps = {};
 
 ModalSM.propTypes = {
-  title: PropTypes.string,
-  closeModal: PropTypes.func,
-  children: PropTypes.element
+	title: PropTypes.string,
+	closeModal: PropTypes.func,
+	children: PropTypes.element
 };
